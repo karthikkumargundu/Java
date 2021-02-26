9fe9e9ce448aa75b04f6639e6e4f64721f2aebec
karthikkumargundu <78849193+karthikkumargundu@users.noreply.github.com>
2021-02-26 16:16:55 +0530

    Update EmployeeMain.java

--- Day2Activity/EmployeeMain.java
+++ Day2Activity/EmployeeMain.java
@@ -3,9 +3,9 @@ public class EmployeeMain {
 	public static void main(String args[])
 	{
 		Employee obj = new Employee();
-		obj.setName("Sandeep");
-		obj.setAddress("Hyderabad");
-		obj.setMobile("6580964571");
+		obj.setName("Karthik kumar");
+		obj.setAddress("Warangal");
+		obj.setMobile("9592336132");
 		System.out.println(obj.getName());
 		System.out.println(obj.getAddress());
 		System.out.println(obj.getMobile());
